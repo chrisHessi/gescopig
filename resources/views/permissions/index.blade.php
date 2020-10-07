7cdefab6bcc23e78a642acc028d112a680ff1f0e
ChristianHessi <hessisan@gmail.com>
2020-10-07 10:37:20 +0100

    modif pour la gestion des notes

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -40,21 +40,3 @@
     </div>
 
 @endsection
-
-@section('scripts')
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(function(){
-            $('.table').DataTable({
-                responsive: true,
-                dom:'Blfrtip',
-                buttons:[
-                    'excel', 'copy'
-                ],
-            })
-        })
-    </script>
-@endsection
