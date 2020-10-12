5f956fd26e1dedd152c202b5b56d5f3b25883df3
ChristianHessi <hessisan@gmail.com>
2020-10-13 00:55:31 +0100

    reference pigier yaounde sur PV

--- resources/views/notes/pv.blade.php
+++ resources/views/notes/pv.blade.php
@@ -267,7 +267,7 @@
         </div>
         <div class=" fixed-bottom">
             <br><br><br><br><br><br><br><br><br><br><br>
-            <h4 class="pull-right"><strong>{{ 'PIG/'.(env('APP_URL') == 'https://www.gescopigyaounde.com' ? 'YDE' : 'DLA') }}</strong></h4>
+            <h4 class="pull-right"><strong>{{ 'PIG/'.(url()->current() == 'https://www.gescopigyaounde.com' ? 'YDE' : 'DLA') }}</strong></h4>
         </div>
 
     </div>
