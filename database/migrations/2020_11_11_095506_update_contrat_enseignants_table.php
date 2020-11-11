b30fcb9e65f999592ea493c9e9ec67484336ad5c
ChristianHessi <hessisan@gmail.com>
2020-11-12 00:53:34 +0100

    mise a jour contrat

---
+++ database/migrations/2020_11_11_095506_update_contrat_enseignants_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateContratEnseignantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contrat_enseignants', function (Blueprint $table) {
+            $table->integer('rang')->nullable();
+        });
+
+        Schema::table('enseignants', function (Blueprint $table) {
+            $table->date('date_naissance')->nullable();
+            $table->string('lieu_naissance')->nullable();
+            $table->string('domicile')->nullable();
+            $table->string('nationalite')->nullable();
+            $table->string('profession')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('enseignants', function (Blueprint $table) {
+            $table->dropColumn('date_naissance')->nullable();
+            $table->dropColumn('lieu_naissance')->nullable();
+            $table->dropColumn('domicile')->nullable();
+            $table->dropColumn('nationalite')->nullable();
+            $table->dropColumn('profession')->nullable();
+        });
+
+        Schema::table('contrat_enseignants', function (Blueprint $table) {
+            $table->dropColumn('rang')->nullable();
+        });
+    }
+}
