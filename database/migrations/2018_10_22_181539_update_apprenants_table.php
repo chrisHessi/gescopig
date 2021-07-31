56f80d9924e0ec48c08e92c21651e4c62060b92c
ChristianHessi <hessisan@gmail.com>
2021-07-31 13:28:32 +0100

    Mise à jour de l'ordre Migrations

--- database/migrations/2018_10_22_181539_update_apprenants_table.php
+++ database/migrations/2018_10_22_181539_update_apprenants_table.php
@@ -13,9 +13,22 @@ class UpdateApprenantsTable extends Migration
      */
     public function up()
     {
+
+        Schema::disableForeignKeyConstraints();
         Schema::table('apprenants', function (Blueprint $table) {
+            $table->dropForeign('apprenants_tutor_id_foreign');
+            $table->dropColumn('tutor_id');
+            $table->string('academic_mail')->nullable();
+            $table->string('diplome')->nullable();
+            $table->string('addresse')->nullable();
+            $table->string('situation_professionnelle')->nullable();
             $table->string('etablissement_provenance');
+
         });
+        Schema::table('tutors', function (Blueprint $table) {
+            $table->foreign('apprenant_id')->references('id')->on('apprenants');
+        });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
--- database/migrations/2019_08_23_085548_update_apprenants_table.php
+++ database/migrations/2019_08_23_085548_update_apprenants_table.php
@@ -4,39 +4,39 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateApprenantsTable2 extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-
-        Schema::disableForeignKeyConstraints();
-        Schema::table('apprenants', function (Blueprint $table) {
-            $table->dropForeign('apprenants_tutor_id_foreign');
-            $table->dropColumn('tutor_id');
-            $table->string('academic_mail')->nullable();
-            $table->string('diplome')->nullable();
-            $table->string('addresse')->nullable();
-            $table->string('situation_professionnelle')->nullable();
-
-        });
-        Schema::table('tutors', function (Blueprint $table) {
-            $table->foreign('apprenant_id')->references('id')->on('apprenants');
-        });
-        Schema::enableForeignKeyConstraints();
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
-}
+//class UpdateApprenantsTable2 extends Migration
+//{
+//    /**
+//     * Run the migrations.
+//     *
+//     * @return void
+//     */
+//    public function up()
+//    {
+//
+//        Schema::disableForeignKeyConstraints();
+//        Schema::table('apprenants', function (Blueprint $table) {
+//            $table->dropForeign('apprenants_tutor_id_foreign');
+//            $table->dropColumn('tutor_id');
+//            $table->string('academic_mail')->nullable();
+//            $table->string('diplome')->nullable();
+//            $table->string('addresse')->nullable();
+//            $table->string('situation_professionnelle')->nullable();
+//
+//        });
+//        Schema::table('tutors', function (Blueprint $table) {
+//            $table->foreign('apprenant_id')->references('id')->on('apprenants');
+//        });
+//        Schema::enableForeignKeyConstraints();
+//    }
+//
+//    /**
+//     * Reverse the migrations.
+//     *
+//     * @return void
+//     */
+//    public function down()
+//    {
+//        //
+//    }
+//}
